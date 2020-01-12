0f820d0f3387e33bf43d8ce80d2edda2aeb6e4da
Thomas Dahlberg <thomasdahlberg@Thomass-MacBook-Pro.local>
2020-01-12 14:14:44 -0500

    ajax jobsite search functional

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,13 @@ $('form').append(`<input type="text" name="latitude" value="${pos.coords.latitud
                 <label>Longitude</label>`);
 }
 
+function showSearchResults() {
+    for(let i = 0; i < newJobsites.length; i++) {
+    $('#search-results').append(`<td>${newJobsites[i].name}</td>`);
+    }
+}
+
+
 if($('#session-submit').val() === 'Clock In'){
     navigator.geolocation.getCurrentPosition(success);  
 }
@@ -20,24 +27,23 @@ if($('#session-submit').val() === 'Clock Out'){
     navigator.geolocation.getCurrentPosition(success);  
 }
 
-// $('#site-search-click').on('click', (event)=> {
-//     event.preventDefault();
-//     const regex = / /gi;
-//     userInput = $('#site-search-data').val().replace(regex, '+');
-//     console.log(userInput);
-//     $.ajax({
-//         url: `http://cors-anywhere.herokuapp.com/${placesURL}`,
-//         type: "GET",
-//     }).then(function(data){
-//         newJobsites = data.results;
-//         showSearchResults();
-//         console.log(newJobsites);
-//     });
-// });
-
-// function showSearchResults(){
-//     for(let i = 0; i < newJobsites.length; i++){
-//     $('#search-results').append(`<td>${newJobsites[i].name}</td>`);
-//     }
-// }
-
+$('#site-search-click').on('click', (event)=> {
+    event.preventDefault();
+    const regex = / /gi;
+    userInput = $('#site-search-data').val().replace(regex, '+');
+    console.log(userInput);
+    $.ajax({
+        url: `http://localhost:3000/api`,
+        type: "GET"
+    }).then(function(data){
+        let placesURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${userInput}&key=${data}`
+        $.ajax({
+            url: `http://cors-anywhere.herokuapp.com/${placesURL}`,
+            type: "GET",
+        }).then(function(data){
+            newJobsites = data.results;
+            showSearchResults();
+            console.log(newJobsites);
+        });
+    });
+});
